2ad6c120fd641bb07210bea7e24bd8468696d20c
Olga Rosas <olga.rosas@gmail.com>
2019-06-10 19:50:41 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
-    binding.pry
-    keys_of.each do |key, value|
+      keys_of.each do |key, value|
+        binding.pry
     end
   end
 end
